088524869625d57b9bd5e1c71c6a11350d364f0c
devaradinata <dinatadevara@gmail.com>
2024-01-04 22:56:12 +0800

    yoo

--- app/src/main/java/com/example/banksampah/kalkulator.kt
+++ app/src/main/java/com/example/banksampah/kalkulator.kt
@@ -3,7 +3,7 @@ package com.example.banksampah
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class home : AppCompatActivity() {
+class kalkulator : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
