fc18af598e3417789e9acef6cf4a0f04d119fa54
ynd <yandepgy123@gmailcom>
2024-01-11 10:45:17 +0700

    UBAH PROFIL

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
     signingConfigs {
         getByName("debug") {
             storeFile =
-                file("G:\\Android\\Android Studio Project\\Bank Sampah\\MyKeystore\\keystore.jks")
+                file("C:\\Users\\warta\\AndroidStudioProjects\\Bank_Sampah\\MyKeystore\\keystore.jks")
             storePassword = "SulihYandeGungDevara"
             keyAlias = "key0"
             keyPassword = "SulihYandeGungDevara"
@@ -52,6 +52,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-database:20.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
