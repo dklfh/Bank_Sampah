53e1e8428c5ae98a896410f4002aef3388088dd4
AgungMantra <agung.mantra07@gmail.com>
2024-01-11 13:10:03 +0800

    nota layout

---
+++ app/src/main/java/com/example/banksampah/nota.kt
@@ -0,0 +1,11 @@
+package com.example.banksampah
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class nota : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nota)
+    }
+}
