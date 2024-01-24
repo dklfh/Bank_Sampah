80664797edf350b121062fa8d97a108915f203e0
dklfh <sulihmragil@gmail.com>
2024-01-25 00:11:29 +0800

    final nav

--- app/src/main/java/com/example/banksampah/cobanavbar.kt
+++ app/src/main/java/com/example/banksampah/cobanavbar.kt
@@ -1,44 +1,113 @@
 package com.example.banksampah
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.TextView
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.navigation.Navigation
-import androidx.navigation.ui.NavigationUI
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+//import androidx.fragment.app.replace
+import com.google.android.material.navigation.NavigationView
 
-@Suppress("DEPRECATION")
-class cobanavbar : AppCompatActivity() {
+class cobanavbar : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var tempat:DrawerLayout
+    private lateinit var toolbarTitle: TextView
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cobanavbar)
 
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        //versi terbaru
+        tempat = findViewById<DrawerLayout>(R.id.tempat)
+
+        val  toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
-        val tempat: DrawerLayout = findViewById(R.id.tempat)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
 
-        val navController = Navigation.findNavController(this, R.id.fragment_container)
-        val navView: com.google.android.material.navigation.NavigationView = findViewById(R.id.nav_view)
-        NavigationUI.setupWithNavController(navView, navController)
-        NavigationUI.setupActionBarWithNavController(this, navController, tempat)
+        toolbarTitle = findViewById<TextView>(R.id.toolbar_title)
 
-
+        val  navigationView = findViewById<NavigationView>(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
 
         val toggle = ActionBarDrawerToggle(this, tempat, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
         tempat.addDrawerListener(toggle)
         toggle.syncState()
-    }
 
-    @Deprecated("Deprecated in Java")
-    override fun onBackPressed() {
-        val tempat: DrawerLayout = findViewById(R.id.tempat)
-        if (tempat.isDrawerOpen(GravityCompat.START)) {
-            tempat.closeDrawer(GravityCompat.START)
-        } else {
-            super.onBackPressed()
+        if (savedInstanceState == null){
+            replaceFragment(coba1())
+            toolbarTitle.text = "Kalkulator"
+            navigationView.setCheckedItem(R.id.nav_kalkulator)
         }
     }
 
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.nav_kalkulator -> {
+                replaceFragment(coba1())
+                toolbarTitle.text = "Kalkulator"
+            }
+            // Data
+            R.id.nav_DatSatuan -> {
+                replaceFragment(coba1())
+                toolbarTitle.text = "Data Satuan"
+            }
+            R.id.nav_DatKategori -> {
+                replaceFragment(coba2())
+                toolbarTitle.text = "Data Kategori"
+            }
+            R.id.nav_DatSub -> {
+                replaceFragment(coba3())
+                toolbarTitle.text = "Data Sub-Kategori"
+            }
+
+            // Laporan
+            R.id.nav_LapSatuan -> {
+                replaceFragment(coba3())
+                toolbarTitle.text = "Laporan Satuan"
+            }
+            R.id.nav_LapKategori -> {
+                replaceFragment(coba3())
+                toolbarTitle.text = "Laporan Kategori"
+            }
+            R.id.nav_LapSub -> {
+                replaceFragment(coba3())
+                toolbarTitle.text = "Laporan Sub-Kategori"
+            }
+
+            // Pengaturan (masih coba2)
+            R.id.nav_hapus -> {
+                replaceFragment(coba3())
+                toolbarTitle.text = "judul"
+            }
+            R.id.nav_setting -> {
+                replaceFragment(coba3())
+                toolbarTitle.text = "judul"
+            }
+        }
+        tempat.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, fragment)
+        transaction.commit()
+    }
+
+    @Deprecated("Deprecated in Java")
+    @SuppressLint("MissingSuperCall")
+    override fun onBackPressed() {
+        if (tempat.isDrawerOpen(GravityCompat.START)){
+            tempat.closeDrawer(GravityCompat.START)
+        } else {
+            onBackPressedDispatcher.onBackPressed()
+        }
+    }
 }
