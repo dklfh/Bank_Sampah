e77c77c487b41cebf0cbcb3d1de19977ce5fcfe5
dklfh <sulihmragil@gmail.com>
2024-01-27 02:53:28 +0800

    actionbar udh selesai

--- app/src/main/java/com/example/banksampah/cobanavbar.kt
+++ app/src/main/java/com/example/banksampah/cobanavbar.kt
@@ -1,9 +1,11 @@
 package com.example.banksampah
 
 import android.annotation.SuppressLint
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.WindowManager
 import android.widget.TextView
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
