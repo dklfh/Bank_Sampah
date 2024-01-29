ab2f504fa5650abbaea62e8d6a7852f71618d7d0
AgungMantra <agung.mantra07@gmail.com>
2024-01-29 14:28:42 +0800

    Semua fragment laporan

--- app/src/main/java/com/example/banksampah/datakategorii.kt
+++ app/src/main/java/com/example/banksampah/datakategorii.kt
@@ -5,57 +5,42 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
 
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [datakategorii.newInstance] factory method to
- * create an instance of this fragment.
- */
 class datakategorii : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-//    val player = arrayOf("Ronaldo","messi","siuuuu","MBAPEEEEE")
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    // Variabel player
+    private val player = arrayOf("Player 1", "Player 2", "Player 3")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_datakategorii, container, false)
-    }
+        val rootView = inflater.inflate(R.layout.fragment_datakategorii, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment datakategorii.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            datakategorii().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+        // Inisialisasi Spinner
+        val spinner = rootView.findViewById<Spinner>(R.id.spinner1)
+        val arrayAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_dropdown_item, player)
+        spinner.adapter = arrayAdapter
+        spinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                Toast.makeText(requireContext(), "Selected player is = ${player[position]}", Toast.LENGTH_SHORT).show()
             }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // Not implemented
+            }
+        }
+
+        return rootView
     }
 }
