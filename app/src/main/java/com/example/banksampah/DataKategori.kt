2bce2a8d833f45e0c5374996f815c24e9fd3cf8b
ynd <yandepgy123@gmailcom>
2024-01-29 11:34:45 +0700

    ssssss

--- app/src/main/java/com/example/banksampah/DataKategori.kt
+++ app/src/main/java/com/example/banksampah/DataKategori.kt
@@ -1,20 +1,31 @@
 package com.example.banksampah
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.Spinner
 import android.widget.Toast
 
 
 class DataKategori : AppCompatActivity() {
+    private lateinit var openButton: Button
 val player = arrayOf("Ronaldo","messi","siuuuu","MBAPEEEEE")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_data_kategori)
+        openButton = findViewById(R.id.openButton)
+
+        // Open new activity on button click
+        openButton.setOnClickListener {
+            val intent = Intent(this, profil::class.java)
+            startActivity(intent)
+        }
+
 
         val spinner = findViewById<Spinner>(R.id.spinner)
         val arrayAdapter = ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,player)
--- app/src/main/java/com/example/banksampah/DataSatuan.kt
+++ app/src/main/java/com/example/banksampah/DataSatuan.kt
@@ -1,11 +1,18 @@
 package com.example.banksampah
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class DataSatuan : AppCompatActivity() {
+    private lateinit var openButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_data_satuan)
+
+
     }
+
 }
