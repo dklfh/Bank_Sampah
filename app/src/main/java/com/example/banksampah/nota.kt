c6e694f804db4a3f19a5aafa8717cbd739b14b88
dklfh <sulihmragil@gmail.com>
2024-02-01 01:32:56 +0800

    struk 0.1

--- app/src/main/java/com/example/banksampah/nota.kt
+++ app/src/main/java/com/example/banksampah/nota.kt
@@ -1,11 +1,21 @@
 package com.example.banksampah
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
 
 class nota : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nota)
+
+        setContentView(R.layout.activity_nota)
+        val cancelButton: Button = findViewById(R.id.cancelbtn)
+        cancelButton.setOnClickListener {
+            val intent = Intent(this, cobanavbar::class.java)
+            startActivity(intent)
+        }
     }
 }
