678845aedcd5c9f23e235620824db64eb717ac55
AgungMantra <agung.mantra07@gmail.com>
2024-01-31 15:03:54 +0800

    semoga mau

--- app/src/main/java/com/example/banksampah/overlayperiode.kt
+++ app/src/main/java/com/example/banksampah/overlayperiode.kt
@@ -2,7 +2,11 @@ package com.example.banksampah
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.Spinner
 
 interface OverlayListener {
     fun onHideOverlay()
@@ -10,6 +14,7 @@ interface OverlayListener {
 
 class OverlayPeriode : AppCompatActivity() {
     private var overlayListener: OverlayListener? = null
+    private val periode = arrayOf("1 Bulan", "6 Bulan", "1 Tahun")
 
     fun setOverlayListener(listener: OverlayListener) {
         overlayListener = listener
@@ -24,5 +29,18 @@ class OverlayPeriode : AppCompatActivity() {
             overlayListener?.onHideOverlay()
             finish()
         }
+
+        // Inisialisasi Spinner
+        val spinner = findViewById<Spinner>(R.id.periode)
+        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, periode)
+        spinner.adapter = arrayAdapter
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {}
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // Not implemented
+            }
+        }
     }
 }
+
