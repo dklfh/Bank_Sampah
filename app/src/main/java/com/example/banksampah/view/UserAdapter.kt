74473d5286de9f8ab9f8ffa4eefd1fbc283bf7c7
ynd <yandepgy123@gmailcom>
2024-02-05 13:02:14 +0700

    hdhhd

--- app/src/main/java/com/example/banksampah/view/UserAdapter.kt
+++ app/src/main/java/com/example/banksampah/view/UserAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.banksampah.view
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,27 +10,31 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.banksampah.R
 import com.example.banksampah.model.UserData
 
-class UserAdapter(val c: FragmentActivity, val userList:ArrayList<UserData>):RecyclerView.Adapter<UserAdapter.UserViewHolder>()
+class UserAdapter(val c: Context, val userList:ArrayList<UserData>):RecyclerView.Adapter<UserAdapter.UserViewHolder>()
 {
 
     inner class UserViewHolder(val v:View):RecyclerView.ViewHolder(v){
-        val name = v.findViewById<TextView>(R.id.mtitle)
+        var name:TextView
+        init {
+            name = v.findViewById<TextView>(R.id.mtitle)
+    }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val v = inflater.inflate(R.layout.list_item,parent,false)
+        val v  = inflater.inflate(R.layout.list_item,parent,false)
         return UserViewHolder(v)
-    }
-
-    override fun getItemCount(): Int {
-        return userList.size
     }
 
     override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
         val newList = userList[position]
-        holder.name.text=newList.userName
-
+        holder.name.text = newList.userName
     }
+
+    override fun getItemCount(): Int {
+        return  userList.size
+    }
+
+
 
 }
