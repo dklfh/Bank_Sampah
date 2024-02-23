6dda8494e921180231cc4636638e61ae39f1b950
AgungMantra <agung.mantra07@gmail.com>
2024-02-23 11:25:21 +0800

    semoga mau

---
+++ app/src/main/java/com/example/banksampah/LoginCheckService.kt
@@ -0,0 +1,29 @@
+package com.example.banksampah
+
+
+import android.content.Context
+import android.content.Intent
+import androidx.core.app.JobIntentService
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginCheckService : JobIntentService() {
+
+    private val mAuth = FirebaseAuth.getInstance()
+
+    companion object {
+        private const val JOB_ID = 1001
+
+        fun enqueueWork(context: Context, intent: Intent) {
+            enqueueWork(context, LoginCheckService::class.java, JOB_ID, intent)
+        }
+    }
+
+    override fun onHandleWork(intent: Intent) {
+        if (mAuth.currentUser != null) {
+            // User is already logged in, send a broadcast to the MainActivity
+            LocalBroadcastManager.getInstance(this)
+                .sendBroadcast(Intent("com.example.banksampah.LOGIN_SUCCESS"))
+        }
+    }
+}
