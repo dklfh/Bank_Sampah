fdbdd42fae916a2454d741f4c15d386ea7da84fa
AgungMantra <agung.mantra07@gmail.com>
2024-02-27 10:19:04 +0800

    semoga mau

---
+++ app/src/main/java/com/example/banksampah/SessionManager.kt
@@ -0,0 +1,20 @@
+package com.example.banksampah
+import android.content.Context
+
+class SessionManager(private val context: Context) {
+
+    private val prefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+
+    companion object {
+        private const val PREFS_NAME = "LoginPrefs"
+        private const val IS_LOGGED_IN = "isLoggedIn"
+    }
+
+    fun isLoggedIn(): Boolean {
+        return prefs.getBoolean(IS_LOGGED_IN, false)
+    }
+
+    fun setLoggedIn(isLoggedIn: Boolean) {
+        prefs.edit().putBoolean(IS_LOGGED_IN, isLoggedIn).apply()
+    }
+}
